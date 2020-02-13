c8a1b361c28848a030cbd0cda1944827e1cf8dc0
makarov-lesha <makarov.lesha@gmail.com>
2020-02-13 15:20:08 +0200

    Updates

---
+++ precache-manifest.0c0886b21508ad902776c90e35869f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5479fec7fc48d941110fdbe3bdedb4a9",
+    "url": "//first-app-7/index.html"
+  },
+  {
+    "revision": "99283ac344934177017c",
+    "url": "//first-app-7/static/css/main.a9e25a4e.chunk.css"
+  },
+  {
+    "revision": "2074369d2e1b0123c9a5",
+    "url": "//first-app-7/static/js/2.c0344d36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "//first-app-7/static/js/2.c0344d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99283ac344934177017c",
+    "url": "//first-app-7/static/js/main.2dac7e9f.chunk.js"
+  },
+  {
+    "revision": "b250c8971bdf9095c87f",
+    "url": "//first-app-7/static/js/runtime-main.6c3ee36c.js"
+  }
+]);
