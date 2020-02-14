5d2929defee872a584920a5fa46e07f5baf07f9e
makarov-lesha <makarov.lesha@gmail.com>
2020-02-14 17:55:14 +0200

    added number to Stoic Newton

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <h1>My React App 7</h1>
       <h2>Author: Alexey Makarov, continious integration, test AM</h2>
-      <h3>Author:calcscout24.de</h3>
-      <h4>STOIC NEWTON</h4>
+      <h3>Author: www.calcscout24.de</h3>
+      <h4>STOIC NEWTON 0</h4>
     </div>
   );
 }
